6335a981b2df344121c1e1aa5450088a738f1713
diazarm <marceloardiaz@gmail.com>
2023-08-18 12:12:13 -0400

    operadores

---
+++ operadores_aritmeticos/operadores.py
@@ -0,0 +1,13 @@
+#suma + resta - division / multiplicacion * 
+
+#division baja //  lo que hace es redondear para abajo, osea que si el resultado da 2.6 elimina el punto y lo que sigue y entrega un entero.
+division_comun = 12 / 5
+division_baja = 12 // 5
+print(division_comun)
+print(division_baja)
+
+#type()  es como el typeof() de JS 
+
+tipo_de_dato = type(division_baja)
+print(tipo_de_dato)
+
--- tipos-de-datos/datos_compuestos.py
+++ tipos-de-datos/datos_compuestos.py
@@ -3,11 +3,21 @@
 lista = ["hola", "soy marcelo", False, 1.78]  #las listas van entre [ ]  
 tupla = ("hola", "soy marcelo", False, 1.78)  #las tuplas van entre ( )
 lista[2] = "nacana"
-print (tupla)
+print (f"Este es tercer el elemento de la tupla {tupla[3]}")
 
-#Tenemos las listas y tuplas, las tuplas NO se pueden modificar, la listas si
+#conjuntos (set) NO puede haber datos duplicados y NO puedo acceder a los elementos, si! al conjunto completo y NO puedo modificar un elemento. va con { }
 
-lista = ["hola", "soy marcelo", False, 1.78]  #las listas van entre [ ]  
-tupla = ("hola", "soy marcelo", False, 1.78)  #las tuplas van entre ( )
-lista[2] = "nacana"
-print (tupla)
+conjunto = {"hola", "este es el conjunto", False, 1.78}
+#se puede recorrer con un bucle
+print(conjunto)
+
+#diccionario, es como un objeto de JS (dict)   key : value
+
+diccionario = {
+    'nombre' : 'marcelo',
+    'apellido' : 'Diaz',
+    'esta_emocionado' : True,
+    'peso' : 80
+    }
+print(diccionario['apellido'])
+
