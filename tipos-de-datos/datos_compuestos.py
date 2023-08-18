a63b430ee800dc5527eb122febf38a49e3a48fe4
diazarm <marceloardiaz@gmail.com>
2023-08-17 22:04:49 -0400

    python

---
+++ tipos-de-datos/datos_compuestos.py
@@ -0,0 +1,13 @@
+#Tenemos las listas y tuplas, las tuplas NO se pueden modificar, la listas si
+
+lista = ["hola", "soy marcelo", False, 1.78]  #las listas van entre [ ]  
+tupla = ("hola", "soy marcelo", False, 1.78)  #las tuplas van entre ( )
+lista[2] = "nacana"
+print (tupla)
+
+#Tenemos las listas y tuplas, las tuplas NO se pueden modificar, la listas si
+
+lista = ["hola", "soy marcelo", False, 1.78]  #las listas van entre [ ]  
+tupla = ("hola", "soy marcelo", False, 1.78)  #las tuplas van entre ( )
+lista[2] = "nacana"
+print (tupla)
--- variables/variable.py
+++ variables/variable.py
@@ -6,6 +6,7 @@ nombre = "victor"
 nombre += " ariel"
 apellido = "Villareal"
 print(nombre)
+nombre_de_tu_snake_case = "vivora"
 #+= significa a lo que tenias sumale e igualalo a esto 
 #Ahora viene el f sting que es similar al bactics de JS
 
@@ -14,9 +15,9 @@ print(f"hola {nombre} como estas?")  #pero es importante colocarle la f adelante
 #si quiero que una variable no este mas definida solamente coloco del nombre y listo!
 del nombre
 
-#Operadores de pertenencia
-print("lucas" in apellido)  #me indica si esta "lucas" en la variable apellido
+#Operadores de pertenencia  SOLO nos entrega tryu o false
+print("lucas" in apellido)  #me indica si esta "lucas" en la variable apellido  
 print("lucas" not in apellido) #me indica si NO esta en la variable apellido
 
-
+print(nombre_de_tu_snake_case)
 
