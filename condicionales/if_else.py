8ee26a93f0d70a1854730b1559d46f17b68bf932
diazarm <marceloardiaz@gmail.com>
2023-08-21 10:08:00 -0400

    condicionales

--- condicionales/if_else.py
+++ condicionales/if_else.py
@@ -1,6 +1,28 @@
-edad = 16
+edad = 6
 
-if edad >= 18: 
+if edad >35:
+    print("deberias estar en tu casa")
+elif edad >= 18: 
     print("podes pasar")
 else: 
     print(f"No podes pasar tenes {edad} anios") 
+    
+#Primero va la condicion mayor, luego utilizo elif (que seria else if) sino.. else
+
+# el if anidado trabaja mucho con la identacion.
+
+sueldo = 550
+gasto = 700
+
+if sueldo >= 1500: 
+    print("estas bien pa!")
+    if sueldo - gasto < 0 :
+        print("estas gastando demasiado")
+    else: print("estas bien")
+elif sueldo <1500 : 
+    if sueldo >500 & sueldo <1500 :
+        print("ganas bien para latam")
+    else:
+        print("ganas muy poco")
+
+    
