a73f4b87e8044515681cb6a2ef055c1ea4d33ab4
diazarm <marceloardiaz@gmail.com>
2023-08-29 21:30:00 -0400

    inputs

---
+++ inputs/inputs.py
@@ -0,0 +1,5 @@
+#pedir un dato al usuario, simplemente con colocar el nombre a una variable y  colocarle input, es suficiente.
+nombre = input("che maestro, dame tu nombre: ")
+
+#Mostrar el dato (en el ejemplo)
+print(f"el nombre es: {nombre}")
